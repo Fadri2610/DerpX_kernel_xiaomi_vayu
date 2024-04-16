57343df01e32c8f092095cf6e25811926db0ee5a
Fadri2610 <fabiflow102004@gmail.com>
2024-04-16 18:54:08 -0500

    Fix compilation errors due to unnecesary characters

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -60,11 +60,6 @@ static DEFINE_SPINLOCK(suspend_lock);
 #define TAG "msm_adreno_tz: "
 
 #if 1
-static unsigned int adrenoboost = 3;
-#endif
-
-static unsigned int adrenoboost = 10000;
-static unsigned int adrenoboost = 1;
 static unsigned int adrenoboost = 0;
 #endif
 
